77dda5a2f42a261b4b7dbe534968400cff11263a
farvath <farvath10@gmail.com>
2025-06-29 07:11:36 +0530

    updated the institutions name.

--- backend/src/models/Institution.ts
+++ backend/src/models/Institution.ts
@@ -6,6 +6,6 @@ export interface IInstitution extends Document {
 
 const InstitutionSchema = new Schema<IInstitution>({
   name: { type: String, required: true, unique: true },
-}, { versionKey: false, collection: 'instituions' });
+}, { versionKey: false, collection: 'institutions' });
 
 export default mongoose.models.Institution || mongoose.model<IInstitution>('Institution', InstitutionSchema);
